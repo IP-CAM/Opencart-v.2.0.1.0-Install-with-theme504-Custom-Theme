2ee36e22348bbc2b3567bca1c58ebbe0111e4eb4
Marco Foganholo <marco@lojavirtual.digital>
2016-07-14 10:51:57 -0300

    Tradução, Correios e PagSeguro

--- public/admin/config.php
+++ public/admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://opencart52694.lojavirtual.local/admin/');
 define('HTTPS_CATALOG', 'http://opencart52694.lojavirtual.local/');
 
 // DIR
-define('DIR_APPLICATION', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/admin/');
-define('DIR_SYSTEM', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/');
-define('DIR_LANGUAGE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/admin/language/');
-define('DIR_TEMPLATE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/admin/view/template/');
-define('DIR_CONFIG', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/config/');
-define('DIR_IMAGE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/image/');
-define('DIR_CACHE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/cache/');
-define('DIR_DOWNLOAD', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/download/');
-define('DIR_UPLOAD', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/upload/');
-define('DIR_LOGS', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/logs/');
-define('DIR_MODIFICATION', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/modification/');
-define('DIR_CATALOG', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/catalog/');
+define('DIR_APPLICATION', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\admin\\');
+define('DIR_SYSTEM', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\');
+define('DIR_LANGUAGE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\admin\\language\\');
+define('DIR_TEMPLATE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\admin\\view\\template\\');
+define('DIR_CONFIG', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\config\\');
+define('DIR_IMAGE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\image\\');
+define('DIR_CACHE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\cache\\');
+define('DIR_DOWNLOAD', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\download\\');
+define('DIR_UPLOAD', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\upload\\');
+define('DIR_LOGS', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\logs\\');
+define('DIR_MODIFICATION', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\modification\\');
+define('DIR_CATALOG', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\catalog\\');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- public/config.php
+++ public/config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://opencart52694.lojavirtual.local/');
 define('HTTPS_SERVER', 'http://opencart52694.lojavirtual.local/');
 
 // DIR
-define('DIR_APPLICATION', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/catalog/');
-define('DIR_SYSTEM', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/');
-define('DIR_LANGUAGE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/catalog/language/');
-define('DIR_TEMPLATE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/catalog/view/theme/');
-define('DIR_CONFIG', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/config/');
-define('DIR_IMAGE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/image/');
-define('DIR_CACHE', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/cache/');
-define('DIR_DOWNLOAD', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/download/');
-define('DIR_UPLOAD', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/upload/');
-define('DIR_MODIFICATION', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/modification/');
-define('DIR_LOGS', 'C:\Users\mfoganholo\Documents\GitHub\opencart52694\public/system/logs/');
+define('DIR_APPLICATION', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\catalog\\');
+define('DIR_SYSTEM', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\');
+define('DIR_LANGUAGE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\catalog\\language\\');
+define('DIR_TEMPLATE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\catalog\\view\\theme\\');
+define('DIR_CONFIG', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\config\\');
+define('DIR_IMAGE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\image\\');
+define('DIR_CACHE', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\cache\\');
+define('DIR_DOWNLOAD', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\download\\');
+define('DIR_UPLOAD', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\upload\\');
+define('DIR_MODIFICATION', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\modification\\');
+define('DIR_LOGS', 'C:\\Users\\mfoganholo\\Documents\\GitHub\\opencart52694\\public\\system\\logs\\');
 
 // DB
 define('DB_DRIVER', 'mysqli');
